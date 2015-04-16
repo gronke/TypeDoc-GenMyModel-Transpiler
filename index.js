bdc4a36d4a4431fd165669dd92d7c3d812d1c241
Stefan Grönke <stefan@gronke.net>
2015-04-16 12:54:20 +0200

    play with importing from json objects

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-var input = require('../API/output.json');
+//var src = './samples/TimeCapsule.json';
+var src = '../API/output.json';
+
+var input = require(src),
+    translation = require('./config/translation.json');
 
 var XMLWriter = require('xml-writer');
     xw = new XMLWriter;
 
+var verbose = false;
+
 // Create Document
 xw.startDocument('1.0', 'UTF-8');
 xw.writeAttribute('encoding', 'UTF-8');
-xw.startElement('uml:Model');
+/*xw.startElement('uml:Model');
 xw.writeAttribute('xmlns:xmi', 'http://www.omg.org/XMI');
 xw.writeAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
 xw.writeAttribute('xmlns:uml', 'http://www.eclipse.org/uml2/4.0.0/UML');
 xw.writeAttribute('xmi:id', '_MgQ4wOHCEeSyAtfQFgWBxA');
 xw.writeAttribute('name', 'model');
-    
+*/
+
 (function(out) {
 
   function generateRandomId(len) {
@@ -45,6 +52,7 @@ xw.writeAttribute('name', 'model');
     return createElement('packagedElement', attrs, target);
   }
 
+/*
   createElement('packageImport', {
     'xmi:id': '_3KdtA-N7EeSyAtfQFgWBxA'
   });
@@ -54,7 +62,59 @@ xw.writeAttribute('name', 'model');
   });
 
   out.endElement().endElement();
+*/
 
+  function processInput(obj) {
+
+    // lookup translation
+    if(!translation[obj.kind]) {
+      if(verbose)
+        console.error('Kind ' + obj.kind + ' has no translation. Skipping');
+      return;
+    }
+
+    var t = translation[obj.kind],
+        attrs = {};
+
+    var translationValue,
+        elementValue,
+        pattern;
+
+    // do translation
+    Object.keys(t.attrs).forEach(function(translationKey) {
+
+      translationValue = t.attrs[translationKey];
+
+      Object.keys(obj).forEach(function(elementKey) {
+
+        if(elementKey != 'children') {
+          elementValue = obj[elementKey];
+          pattern = new RegExp('{{' + elementKey + '}}')
+          translationValue = translationValue.replace(pattern, elementValue);
+        }
+
+      });
+
+      attrs[translationKey] = translationValue;
+
+    });
+
+    var elementType = t.elementType || 'packagedElement';
+    createElement(elementType, attrs);
+
+    if(obj.children && obj.children.length > 0) {
+      obj.children.forEach(function(child) {
+        processInput(child);
+      });
+    }
+
+    out.endElement();
+
+  }
+
+  processInput(input);
+
+/*
   createElement('packageImport', {
     'xmi:id': '_3KdtBuN7EeSyAtfQFgWBxA'
   });
@@ -79,6 +139,8 @@ xw.writeAttribute('name', 'model');
     }).endElement();
 
   });
+
+*/
 
 })(xw);
 
