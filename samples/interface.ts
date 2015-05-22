e19107611e049fc192f55d363fcc97f17ec89444
Stefan Grönke <stefan@gronke.net>
2015-05-22 16:09:46 +0200

    add interface sample

---
+++ samples/interface.ts
@@ -0,0 +1,26 @@
+interface CarInterface {
+  manufacturer: string;
+  model: string;
+ 
+  wheels: number;
+}
+
+class Car implements CarInterface {
+
+  public manufacturer: string;
+  public model: string;
+  public wheels: number;
+
+  constructor(manufacturer: string, model: string) {
+    this.manufacturer = manufacturer;
+    this.model = model;
+    this.wheels = 4;
+  }
+
+  print() {
+    return '<pre>Manufacturer: ' + this.manufacturer + '\nModel: ' + this.model + '\nWheels: ' + this.wheels + '</pre>';
+  }
+
+}
+
+
